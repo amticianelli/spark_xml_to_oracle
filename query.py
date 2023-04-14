2dfa3a819b14ab0f1129680749ce2f7ddf462586
Alberto M. Ticianelli <67422877+amticianelli@users.noreply.github.com>
2023-04-14 17:17:38 +0100

    Testing

--- query.py
+++ query.py
@@ -311,7 +311,7 @@ class xmlToOracle:
             AND estab_rem.COD_ESTAB LIKE 'BR%'
             AND NFe.infNfe.ide.toma3.toma = '0'
         LEFT JOIN ESTABELECIMENTO estab_toma ON 1=1
-            AND estab_toma.CGC = NVL(XML_RAW_CAPA.NFe.infNfe.toma.CNPJ,XML_RAW_CAPA.NFe.infNfe.toma4.CNPJ)
+            AND estab_toma.CGC = XML_RAW_ITEM.NFe.infNfe.toma.CNPJ
             AND estab_toma.COD_ESTAB LIKE 'BR%'
             AND (
                     NFe.infNfe.ide.toma4.toma = '4'
